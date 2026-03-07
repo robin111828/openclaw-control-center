0647222eef33fe06494510a8efa772d7def2cc29
robin111828 <albbly@sina.com>
2026-03-07 19:51:12 +0800

    refactor: add desktop shell module entry
    Add a shell module entry file to re-export shell state, layout, and navigation from a single location.

---
+++ apps/desktop/src/app/shell/index.ts
@@ -0,0 +1,14 @@
+export {
+  createDesktopShellState,
+  type DesktopShellState,
+} from "./shell-state";
+
+export {
+  createDesktopShellLayout,
+  type DesktopShellLayout,
+} from "./shell-layout";
+
+export {
+  createDesktopNavigationItems,
+  type DesktopNavigationItem,
+} from "./shell-navigation";
